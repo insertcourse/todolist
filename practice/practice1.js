b6e420e75164b479f43b03d912a7985b0bbb6ad9
youngeunkim <pkhj286@gmail.com>
2023-04-30 22:58:48 +0900

    ADD : mission 1 1단계

--- practice/practice1.js
+++ practice/practice1.js
@@ -1,1 +1,19 @@
 // mission 1
+const first = parseInt(prompt("계산할 첫 번째 값 입력:"));
+const second = prompt("계산할 부호 입력 (+, -, *, /, %):");
+const third = parseInt(prompt("계산할 두 번째 값 입력:"));
+let = result;
+
+if (second === "+") {
+    result = first + third;
+} else if (second=== "-") {
+    result = first - third;
+} else if (second === "*") {
+    result = first * third;
+} else if (operator === "/") {
+    result = first / third;
+} else if (operator === "%") {
+    result = first % third;
+}
+
+alert(`${first}${second}${third}는 ${result}입니다.`);
