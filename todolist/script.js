c45ab9a86eb3cc4f740a44f9a7e99f15111e0101
youngeunkim <pkhj286@gmail.com>
2023-05-02 23:16:30 +0900

    REFACTOR : id,class 명 변경, JS 삭제

--- todolist/script.js
+++ todolist/script.js
@@ -1,6 +1,2 @@
 // 화이팅 
 //화이팅팅
-const button = document.getElementsByClassName('inputForm--button');
-let inputForm = document.getElementsByClassName('inputForm--add'); 
-let result = document.getElementsByClassName('addToDo--text');
-
