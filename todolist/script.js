6410e9f2de62d3b8e18c6a0bebf1c38b0d45f0a1
youngeunkim <pkhj286@gmail.com>
2023-05-02 21:29:13 +0900

    ADD : js 변수 선언

--- todolist/script.js
+++ todolist/script.js
@@ -1,3 +1,6 @@
 // 화이팅 
 //화이팅팅
+const button = document.getElementById('inputForm--button');
+let inputForm = document.getElementById('inputForm--add'); 
+let result = document.getElementById('addToDo--text');
 
