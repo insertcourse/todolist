8b2f1b345ca7d77f6a6b460bf5b83b66afab5301
youngeunkim <pkhj286@gmail.com>
2023-05-04 23:17:37 +0900

    ADD : 하나 씩 삭제하기

--- todolist/script.js
+++ todolist/script.js
@@ -9,20 +9,38 @@ function addTodo(){
     if(add.value==false){ //value = input에서 입력 값을 가져올 때 사용하는 것!!!!!!
         alert('내용을 입력하세요!');
     }else{
-    let list = document.createElement("li"); 
-    list.innerText = add.value;
-    result.appendChild(list); 
-    add.value = "";     //입력창 초기화!!
+        let list = document.createElement("li");
+        let del = document.createElement('button');
+        list.innerHTML = add.value;
+        result.appendChild(list);
+        list.appendChild(del);       
+        del.innerText = "x";      
+        del.style.fontSize = "20px"; //js에서 스타일 지정
+        del.style.border = "none";
+        del.style.float = "right";
+        del.style.right = "17px";
+        del.style.margin = "8px 10px 0 0";
+        del.style.cursor = "pointer";
+        del.addEventListener("click", deleteList); 
+
+        add.value = ""; //초기화
+
 }
 }
+
 function enterkey() {
     if (event.keyCode == 13) {
         addTodo(); //enter 키 눌렀을 때 addTodo 함수 실행
     }
 }
 
+function deleteList(e){ //삭제 버튼(x) 클릭시 
+    let remove1 = e.target.parentElement;  //선택한 목록 한개만 지우기
+    remove1.remove();
+}
+
 function allClearList(){
-        result.innerText='';                 
+        result.innerText='';  //할 일 입력햇을 때 입력창 초기화하기     
 }
 
 button.addEventListener('click', addTodo);
