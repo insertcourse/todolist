a5992e7976899f33d29c205680e03eb348938452
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-05-05 15:01:22 +0900

    ADD : 응원메세지 작성

--- todolist/script.js
+++ todolist/script.js
@@ -1,1 +1,16 @@
 // 화이팅
+
+//응원메세지
+const CheerMassage = document.querySelector(".cheer");
+function onclick(){
+    const change = prompt("응원 메세지를 입력해주세용");
+
+    if(change==""){
+        alert("입력되지 않았어요ㅠㅠ 다시 돌아가세요");
+    }else{
+        CheerMassage.innerHTML=change;
+    }
+}
+CheerMassage.addEventListener("click", onclick);
+
+
