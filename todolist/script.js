29255adeb9ec697a57474700e5f56beddc723614
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-05-05 15:03:49 +0900

    ADD : 화요일~일요일 계획 입력

--- todolist/script.js
+++ todolist/script.js
@@ -26,3 +26,87 @@ function onclick(){
     }
 }
 DayMassage1.addEventListener("click", onclick);
+
+
+//화요일 계획
+const DayMassage2 = document.querySelector(".day2--massage");
+function onclick(){
+    const change = prompt("화요일 계획을 써주세요!");
+
+    if(change==""){
+        alert("입력되지 않았어요ㅜㅜ 다시 돌아가세요");
+    }else{
+        DayMassage2.innerHTML=change;
+    }
+}
+DayMassage2.addEventListener("click", onclick);
+
+
+//수요일 계획
+const DayMassage3 = document.querySelector(".day3--massage");
+function onclick(){
+    const change = prompt("수요일 계획을 써주세요!");
+
+    if(change==""){
+        alert("입력되지 않았어요ㅜㅜ 다시 돌아가세요");
+    }else{
+        DayMassage3.innerHTML=change;
+    }
+}
+DayMassage3.addEventListener("click", onclick);
+
+
+//목요일 계획
+const DayMassage4 = document.querySelector(".day4--massage");
+function onclick(){
+    const change = prompt("목요일 계획을 써주세요!");
+
+    if(change==""){
+        alert("입력되지 않았어요ㅜㅜ 다시 돌아가세요");
+    }else{
+        DayMassage4.innerHTML=change;
+    }
+}
+DayMassage4.addEventListener("click", onclick);
+
+
+//금요일 계획
+const DayMassage5 = document.querySelector(".day5--massage");
+function onclick(){
+    const change = prompt("금요일 계획을 써주세요!");
+
+    if(change==""){
+        alert("입력되지 않았어요ㅜㅜ 다시 돌아가세요");
+    }else{
+        DayMassage5.innerHTML=change;
+    }
+}
+DayMassage5.addEventListener("click", onclick);
+
+
+//토요일 계획
+const DayMassage6 = document.querySelector(".day6--massage");
+function onclick(){
+    const change = prompt("토요일 계획을 써주세요!");
+
+    if(change==""){
+        alert("입력되지 않았어요ㅜㅜ 다시 돌아가세요");
+    }else{
+        DayMassage6.innerHTML=change;
+    }
+}
+DayMassage6.addEventListener("click", onclick);
+
+
+//일요일 계획
+const DayMassage7 = document.querySelector(".day7--massage");
+function onclick(){
+    const change = prompt("일요일 계획을 써주세요!");
+
+    if(change==""){
+        alert("입력되지 않았어요ㅜㅜ 다시 돌아가세요");
+    }else{
+        DayMassage7.innerHTML=change;
+    }
+}
+DayMassage7.addEventListener("click", onclick);
